df43c46feefa0dee79bfe78ef09f466b9b77e8a1
fortune710 <fortune0208@yahoo.com>
2024-10-16 11:53:15 -0400

    function to add cart data to supabase

---
+++ src/supabase/index.ts
@@ -0,0 +1,52 @@
+import { Metadata, SupabaseTables } from "../types";
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import { groupCartItemsByRestaurant, parseMetadata } from "../utils";
+
+dotenv.config();
+
+
+// Supabase client initialization
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or Key');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function createOrder(data: Metadata) {
+    const userCart = parseMetadata(data);
+    const orders = groupCartItemsByRestaurant(userCart);
+    
+
+    return await Promise.all(
+        orders.map(async (order) => {
+            const { data } = await supabase.from(SupabaseTables.Orders).upsert({
+                restaurant_id: order.restaurant_id,
+                total_amount: order.total_amount,
+                notes: "",
+                user_paid: false,
+                customer_name: order.customer_name,
+                user_id: order.user_id
+            }).select()
+            const orderData = data![0] as {
+                id: number,
+                total_amount: number,
+                restaurant_id: number,
+            }
+            
+            await supabase.from(SupabaseTables.OrderItems).insert(
+                order.order_items.map((item) => ({
+                    order_id: orderData.id,
+                    menu_item_id: item.menu_item_id,
+                    quantity: item.quantity,
+                }))
+            )
+        })
+    )
+
+    
+
+}
