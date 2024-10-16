bf6d0efd3b32a9a4c4eaaf10c782f627beed7b6b
fortune710 <fortune0208@yahoo.com>
2024-10-16 11:52:36 -0400

    utility types

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export interface CartItem {
+    id: number,
+    menu_item_id: number,
+    menu_items: {
+        description: string|null,
+        name: string,
+        price: number,
+        resturant_id: number
+    },
+    quantity: number
+}
+
+export interface Metadata {
+    user_id: string,
+    customer_name: string,
+    cart: CartItem[]
+}
+
+export interface Order {
+    total_amount: number,
+    restaurant_id: number,
+    user_id: string,
+    customer_name: string,
+    order_items: CartItem[],
+}
+
+
+export enum SupabaseTables {
+    Orders = "orders",
+    OrderItems = "order_items"
+}
